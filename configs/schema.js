aa67df3f548d0b1f461acdbd183e6aff59d0a90f
vardhakkore <125583188+vardhak@users.noreply.github.com>
2025-04-10 12:08:03 +0530

    Update schema.js
    set default user to true (miner bug)

--- configs/schema.js
+++ configs/schema.js
@@ -12,7 +12,7 @@ export const usersTable = pgTable("users", {
   id: serial().primaryKey(),
   name: varchar().notNull(),
   email: varchar().notNull().unique(),
-  ismember: boolean().default(false),
+  ismember: boolean().default(true),
   usedCredits: integer().default(0),
   avilableCredits: integer().default(5),
 });
